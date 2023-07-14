388da2f3bc9ae36f7fcda0618711dd82140900d0
markrajk <marko88rajkovic@gmail.com>
2023-07-14 22:42:04 +0200

    first commit

---
+++ src/lib/firebase.ts
@@ -0,0 +1,96 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { doc, getFirestore, onSnapshot } from 'firebase/firestore';
+import { getAuth, onAuthStateChanged, type User } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+import { derived, writable } from 'svelte/store';
+import type { Readable } from 'svelte/motion';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyDBV27WXiera8s77Fc2fCQBzm-70Uw2N0w',
+	authDomain: 'linktree-clone-2ca30.firebaseapp.com',
+	projectId: 'linktree-clone-2ca30',
+	storageBucket: 'linktree-clone-2ca30.appspot.com',
+	messagingSenderId: '1010121384618',
+	appId: '1:1010121384618:web:6c633a37b15cda59dae6d3',
+	measurementId: 'G-P62C3GZ3VM'
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = getAuth();
+export const storage = getStorage();
+
+/**
+ * @returns a store with the current firebase user
+ */
+function userStore() {
+	let unsubscribe: () => void;
+
+	if (!auth || !globalThis.window) {
+		console.warn('Auth is not initialized or not in browser');
+		const { subscribe } = writable<User | null>(null);
+
+		return {
+			subscribe
+		};
+	}
+
+	const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+		unsubscribe = onAuthStateChanged(auth, (user) => {
+			set(user);
+		});
+
+		return () => unsubscribe();
+	});
+
+	return {
+		subscribe
+	};
+}
+
+export const user = userStore();
+
+/**
+ * @param  {string} path document path or reference
+ * @returns a store with realtime updates on document data
+ */
+export function docStore<T>(path: string) {
+	let unsubscribe: () => void;
+
+	const docRef = doc(db, path);
+
+	const { subscribe } = writable<T | null>(null, (set) => {
+		unsubscribe = onSnapshot(docRef, (snapshot) => {
+			set((snapshot.data() as T) ?? null);
+		});
+
+		return () => unsubscribe();
+	});
+
+	return {
+		subscribe,
+		ref: docRef,
+		id: docRef.id
+	};
+}
+
+interface UserData {
+	username: string;
+	bio: string;
+	photoURL: string;
+	links: any[];
+}
+
+export const userData: Readable<UserData | null> = derived(user, ($user, set) => {
+	if ($user) {
+		return docStore<UserData>(`users/${$user.uid}`).subscribe(set);
+	} else {
+		set(null);
+	}
+});
